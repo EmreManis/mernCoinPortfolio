64caa96971872f2dccdd7a65e39026e2874740a5
EmreManis <emremn07@gmail.com>
2021-08-28 11:16:47 +0300

    nav setup

--- frontendCoinPortfolio/src/components/Navigation/MainNavigation.js
+++ frontendCoinPortfolio/src/components/Navigation/MainNavigation.js
@@ -1,15 +1,18 @@
+import React, { useState } from "react";
+
+
 
 const mainNavigation  = (props) =>{
     return (
-        <nav className="flex bg-red-100">
-            <div className="w-1/4">Coin Portfolio Logo</div>
-            <div className="flex justify-around w-2/4">
+        <nav className="flex bg-red-100 px-1">
+            <div className="w-1/5"><h1 className="text-2xl"><a href="/">Coin <i>Portfolio</i></a></h1></div>
+            <div className="flex justify-end items-center w-3/5 space-x-4">
                 <p>Total Balance: 999 usd</p>
                 <p>Profit / Loss: +132 usd</p>
             </div>
-                <ul className="flex justify-end w-2/4">
-                    <li>Log In</li>
-                    <li>Sign Up</li>
+                <ul className="flex justify-end items-center w-1/5 space-x-4">
+                    <li><a href="/">Log In</a></li>
+                    <li><a href="/">Sign Up</a></li>
                 </ul>
         </nav>
     );
