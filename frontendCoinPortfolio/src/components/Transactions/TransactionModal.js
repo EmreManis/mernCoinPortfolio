e26ba34a79bc5da24435680c34f961e4afec16c0
Emre Manış <emremanis@Emres-MacBook-Air.local>
2021-09-28 22:31:31 +0300

    add defaultVal to select

--- frontendCoinPortfolio/src/components/Transactions/TransactionModal.js
+++ frontendCoinPortfolio/src/components/Transactions/TransactionModal.js
@@ -1,173 +1,194 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import Input from "../../shared/Input";
 import { useForm } from "../../shared/hooks/form-hook";
-import * as validator from "../../shared/Validator"
+import * as validator from "../../shared/Validator";
 
-import DatePicker from 'react-date-picker';
+import DatePicker from "react-date-picker";
 
 import "./DatePicker.css";
 
-
 const TransactionModal = (props) => {
+  const [dummyData, setDummyData] = useState({
+    selectedOption: {
+      value: "etherium",
+      price: 34444,
+    }
+  });
 
+//   ,
+//     bitCoin: {
+//       name: "BitCoin",
+//       pricePerCoin: 46759,
+//     },
+//     bitTorret: {
+//       name: "Bittoret",
+//       pricePerCoin: 7242,
+//     }
 
-        const [formState, inputHandler] = useForm(
-            {
-                        coin:{
-                            value:'',
-                            isValid: true
-                        },
-                        quantity:{
-                            value:'',
-                            isValid: false
-                        },
-                        pricePerCoin:{
-                            value:'',
-                            isValid: true
-                        },
-                        date:{
-                            value:'',
-                            isValid: true
-                        },
-                        fee:{
-                            value:'',
-                            isValid:true
-                        },
-                        notes:{
-                            value:'',
-                            isValid: true
-                        }
-                    },
-                    false
-                    );
+  const [formState, inputHandler] = useForm(
+    {
+      coin: {
+        value: "",
+        isValid: true,
+      },
+      quantity: {
+        value: "",
+        isValid: false,
+      },
+      pricePerCoin: {
+        value: "",
+        isValid: true,
+      },
+      date: {
+        value: "",
+        isValid: true,
+      },
+      fee: {
+        value: "",
+        isValid: true,
+      },
+      notes: {
+        value: "",
+        isValid: true,
+      },
+    },
+    false
+  );
 
+  const [value, onChange] = useState(new Date());
 
-        const [value, onChange] = useState(new Date());
+  const changedHandler = (event, price) => {
+    console.log(price);
+      setDummyData({
+          value: event.target.value
+      });
+      
+  };
 
-        console.log(formState);
+  console.log(dummyData);
+  return (
+    <form className="w-full max-w-lg">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Coin Name
+          </label>
+          <div className="relative">
+            <select
+              onChange={(event) =>changedHandler(event, 36666)}
+              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="coin-name"
+            >
+              <option value="etherium">Etherium</option>
+              <option value="bitCoin">BitCoin</option>
+              <option value="bitTorret">BitTorret</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-between -mx-3 mb-2">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Quantity
+          </label>
+          <Input
+            element="input"
+            validators={[validator.VALIDATOR_REQUIRE]}
+            onInput={inputHandler}
+            errorMessage="Enter a valid Quantity"
+            cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="quantity"
+            type="number"
+            placeholder="0.00"
+            min="0"
+          />
+        </div>
 
-        return (
-            <form className="w-full max-w-lg">
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                            <div className="w-full px-3">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Coin Name
-                                    </label>
-                                    <div className="relative">
-                                            <select
-                                                onChange={inputHandler}
-                                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                                id="coin-name">
-                                                    <option value="etherium">Etherium</option>
-                                                    <option value="bitCoin">BitCoin</option>
-                                                    <option value="bitTorret">BitTorret</option>
-                                            </select>
-                                            <div
-                                                className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                                    <svg className="fill-current h-4 w-4"
-                                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                            <path
-                                                                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
-                                                    </svg>
-                                            </div>
-                                    </div>
-                            </div>
-                    </div>
-                    <div className="flex flex-wrap justify-between -mx-3 mb-2">
-                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Quantity
-                                    </label>
-                                    <Input
-                                        element="input"
-                                        validators={[validator.VALIDATOR_REQUIRE]}
-                                        onInput={inputHandler}
-                                        errorMessage="Enter a valid Quantity"
-                                        cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="quantity" type="number" placeholder="0.00" min="0"/>
-                            </div>
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Price Per Coin
+          </label>
+          <Input
+            validators={[]}
+            element="input"
+            onInput={inputHandler}
+            cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            type="number"
+            id="pricePerCoin"
+            value={34444}
+            valid={true}
+          />
+        </div>
+      </div>
+      <div className="flex -mx-3 mb-2">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Date
+          </label>
 
-                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Price Per Coin
-                                    </label>
-                                    <Input
-                                        validators={[]}
-                                        element="input"
-                                        onInput={inputHandler}
-                                        cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        type="number"
-                                        id="pricePerCoin"
-                                        />
-                            </div>
-                    </div>
-                    <div className="flex -mx-3 mb-2">
-                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Date
-                                    </label>
+          <DatePicker className="wrapper" onChange={onChange} value={value} />
+        </div>
 
-                                            <DatePicker
-                                                className="wrapper"
-                                                onChange={onChange}
-                                                value={value}
-                                            />
-                            </div>
-
-                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Fee
-                                    </label>
-                                    <Input
-                                        id="fee"
-                                        validators={[]}
-                                        element="input"
-                                        onInput={inputHandler}
-                                        cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                        id="fee" type="number" min="0"/>
-                            </div>
-                    </div>
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                    <label
-                                        className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                            Notes
-                                    </label>
-                                    <Input
-                                            validators={[]}
-                                            id="notes"
-                                            onInput={inputHandler}
-                                            element="textarea"
-                                            cssClass="resize-none block appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                            rows="4"
-                                            cols="50"
-                                            placeholder="Add Note"/>
-
-                            </div>
-                    </div>
-                    <div className="flex -mx-px mb-6">
-                            <div className="w-full block bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight">
-                                    <span  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Total Spent: 5$</span>
-                            </div>
-
-                    </div>
-                    <div className="flex justify-center">
-                            <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                    disabled={!formState.isValid ? "opacity-50 cursor-not-allowed" : ""}>
-                                Add Transaction
-                            </button>
-                    </div>
-            </form>
-        )
-}
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Fee
+          </label>
+          <Input
+            id="fee"
+            validators={[]}
+            element="input"
+            onInput={inputHandler}
+            cssClass="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="fee"
+            type="number"
+            min="0"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Notes
+          </label>
+          <Input
+            validators={[]}
+            id="notes"
+            onInput={inputHandler}
+            element="textarea"
+            cssClass="resize-none block appearance-none bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            rows="4"
+            cols="50"
+            placeholder="Add Note"
+          />
+        </div>
+      </div>
+      <div className="flex -mx-px mb-6">
+        <div className="w-full block bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight">
+          <span className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Total Spent: 5$
+          </span>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <button
+          className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={!formState.isValid ? "opacity-50 cursor-not-allowed" : ""}
+        >
+          Add Transaction
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default TransactionModal;
-
-
-
--- frontendCoinPortfolio/src/shared/Input.js
+++ frontendCoinPortfolio/src/shared/Input.js
@@ -3,78 +3,82 @@ import React, { useReducer, useEffect } from "react";
 import { validate } from "./Validator";
 
 const inputReducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE':
-            return {
-                ...state,
-                value: action.val,
-                isValid: validate(action.val, action.validators)
-            };
-        case 'TOUCH':
-            return {
-                ...state,
-                isTouched: true
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
 
-const Input = props => {
+const Input = (props) => {
+  const cssStyle =
+    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
 
-    const cssStyle = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline";
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.value || "",
+    isValid: props.valid || false,
+    isTouched: false,
+  });
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
 
-    const [inputState, dispatch] = useReducer(inputReducer, {value:'', isValid: false, isTouched: false});
-    const { id, onInput } = props;
-    const { value, isValid } = inputState;
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
 
-    useEffect(() => {
-            onInput(id, value, isValid)
-    }, [id, value, isValid, onInput]);
+  const changedHandler = (event) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
 
-    const changedHandler = (event) => {
-        dispatch(
-            {
-                type: 'CHANGE',
-                val: event.target.value,
-                validators: props.validators
-            });
-    }
+  const touchedHandler = () => {
+    dispatch({ type: "TOUCH" });
+  };
 
-    const touchedHandler = () => {
-        dispatch({type: 'TOUCH'});
-    }
+  const element =
+    props.element === "input" ? (
+      <input
+        className={`${cssStyle}` + " " + props.cssClass}
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        value={inputState.value}
+        onChange={changedHandler}
+        onBlur={touchedHandler}
+      />
+    ) : (
+      <textarea
+        className={props.cssClass}
+        id={props.id}
+        placeholder={props.placeholder}
+        value={inputState.value}
+        onChange={changedHandler}
+        rows={props.rows}
+        cols={props.cols}
+      />
+    );
 
-    const element =
-        props.element === "input" ? (
-            <input
-                className={`${cssStyle}`+" "+ props.cssClass}
-                id={props.id}
-                type={props.type}
-                placeholder={props.placeholder}
-                value={inputState.value}
-                onChange={changedHandler}
-                onBlur={touchedHandler}/>
-        ):(
-            <textarea
-                className={props.cssClass}
-                id={props.id}
-                placeholder={props.placeholder}
-                value={inputState.value}
-                onChange={changedHandler}
-                rows={props.rows}
-                cols={props.cols}
-            />
-        );
-
-        return (
-            <React.Fragment>
-                {element}
-                {inputState.isTouched && !inputState.isValid && <p className="text-red-600">{props.errorMessage}</p>}
-            </React.Fragment>
-
-        );
-
-}
+  return (
+    <React.Fragment>
+      {element}
+      {inputState.isTouched && !inputState.isValid && (
+        <p className="text-red-600">{props.errorMessage}</p>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Input;
