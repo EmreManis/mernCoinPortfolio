e9956328f1853e2f81c86dd010cbc35d16e77c93
Emre <emremn07@gmail.com>
2021-10-19 21:32:34 +0300

    dummy portfolio setup

--- backendCoinPortfolio/routes/transactions-routes.js
+++ backendCoinPortfolio/routes/transactions-routes.js
@@ -2,45 +2,46 @@ const express = require("express");
 
 const router = express.Router();
 
-
-const DummyPortfolio = {
-  portfolio: [
-    {
-      rank: "1",
-      name: "Bitcoin",
-      price: "$49.403.256",
-      oneHour: "-1.14",
-      daily: "1.40",
-      weekly: "4.30",
-      volume: "$50.000",
-      mktCap: "$42.395.476.421",
-      lastWeek: "graph",
-    },
-    {
-      rank: "2",
-      name: "Etherium",
-      price: "$3.403.256",
-      oneHour: "1.58",
-      daily: "-4.40",
-      weekly: "4.30",
-      volume: "$50.473",
-      mktCap: "$395.476.421",
-      lastWeek: "graph",
-    },
-    {
-      rank: "3",
-      name: "Cona",
-      price: "$3.403.256",
-      oneHour: "1.58",
-      daily: "-4.40",
-      weekly: "4.30",
-      volume: "$50.473",
-      mktCap: "$395.476.421",
-      lastWeek: "graph",
-    },
-  ],
-  user: "p1",
-};
+const DummyPortfolio = [
+  {
+    id: 'user1',
+    portfolio: [
+      {
+        rank: "1",
+        name: "Bitcoin",
+        price: "$49.403.256",
+        oneHour: "-1.14",
+        daily: "1.40",
+        weekly: "4.30",
+        volume: "$50.000",
+        mktCap: "$42.395.476.421",
+        lastWeek: "graph",
+      },
+      {
+        rank: "2",
+        name: "Etherium",
+        price: "$3.403.256",
+        oneHour: "1.58",
+        daily: "-4.40",
+        weekly: "4.30",
+        volume: "$50.473",
+        mktCap: "$395.476.421",
+        lastWeek: "graph",
+      },
+      {
+        rank: "3",
+        name: "Cona",
+        price: "$3.403.256",
+        oneHour: "1.58",
+        daily: "-4.40",
+        weekly: "4.30",
+        volume: "$50.473",
+        mktCap: "$395.476.421",
+        lastWeek: "graph",
+      },
+    ],
+  },
+];
 
 router.get("/", (req, res, next) => {
   console.log("Get request in tablebuilder");
@@ -48,7 +49,12 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/portfolio/:uid", (req, res, next) => {
+  const userId = req.params.uid;
 
+  const portf = DummyPortfolio.find(p => {
+      return p.id === userId;
+  });
+  res.json({portf});
 });
 
 module.exports = router;
