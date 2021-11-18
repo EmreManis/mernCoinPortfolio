aab46d706ffa9a7cde4da5effb449cd794efd9eb
Emre <emremn07@gmail.com>
2021-11-19 01:13:11 +0300

    added login route

--- backendCoinPortfolio/controllers/user-controllers.js
+++ backendCoinPortfolio/controllers/user-controllers.js
@@ -1,28 +1,39 @@
-const { v4:uuid }= require("uuid");
+const { v4: uuid } = require("uuid");
+
+const HttpError = require("../models/http-error");
 
 const DummyUsers = [
-    {
-        id: "u1",
-        email: "test@test.com",
-        password: "test"
-    }
+  {
+    id: "u1",
+    email: "test@test.com",
+    password: "test",
+  },
 ];
 
 const userSignUp = (req, res, next) => {
-    const { email, password } = req.body;
-    
-    const createdUser = {
-        id: uuid(),
-        email,
-        password
-    }
+  const { email, password } = req.body;
 
-    DummyUsers.push(createdUser);
+  const createdUser = {
+    id: uuid(),
+    email,
+    password,
+  };
 
-    res.status(201).json({user: createdUser});
+  DummyUsers.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
 };
 
-const userLogin = (req, res, next) => {};
+const userLogin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DummyUsers.find((u) => u.email === email);
+  if(!identifiedUser || identifiedUser.password !== password) {
+      throw new HttpError("Could not identify user, credentials seem to be wrong", 401);
+  }
+
+  res.status(201).json({message: "Logged in"});
+};
 
 exports.userSignUp = userSignUp;
 exports.userLogin = userLogin;
